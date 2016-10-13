f7666b05f1844b3fc72b17fee54619d0e3aad2cb
Janne Hamalainen <janne.a.hamalainen@gmail.com>
2016-10-13 22:36:30 +0300

    Initial version working

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import cats from './cats';
+import component from './component';
 import $ from 'jquery';
 
 $('<h1>Cats</h1>').appendTo('body');
@@ -7,3 +8,7 @@ const ul = $('<ul></ul>').appendTo('body');
 for (const cat of cats) {
     $('<li></li>').text(cat).appendTo(ul);
 }
+
+const compy = new component();
+compy.mount();
+
